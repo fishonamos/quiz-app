572c611701b6dd3bd541b92a3ab997dc0a8b412e
amosfishon <amosfishon@gmail.com>
2022-12-13 23:50:22 +0100

    Updated the questions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 print("Welcome GitHub Quiz!")
 
+
 score = 0
 
 question_1 = input("What is another name for Repository in GitHub?\nA. Mainfile\nB. File\nC. Folder\n")
@@ -14,20 +15,21 @@ else:
 
 question_2 = input("What is GIT ?\nA. It is a CMD app\nB. version control system\nC. A coding storage\n")
 
-if question_2 == "A":
+if question_2 == "B" or 'b':
     score += 10
     print("Correct!")
 else:
     print("Incorrect. The correct answer is version control system.")
 
-question_3 = input("In what year did India gain independence?\nA. 1947\nB. 1950\nC. 1952\n")
+question_3 = input("How can you stage a new file on GIT?\nA. git add [filename]\nB. git commmit -m 'message'\nC. git shiftx\n")
 
-if question_3 == "A":
+if question_3 == "A" or 'a':
     score += 10
     print("Correct!")
 else:
-    print("Incorrect. The correct answer is 1947.")
+    print("Incorrect. The correct answer is git add [filename].")
 
 print("Your final score is:", score)
 
 input = input("Press the Enter key to exit")
+
