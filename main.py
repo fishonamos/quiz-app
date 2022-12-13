401356518856778fd18b35c5352152065aab4c4f
amosfishon <amosfishon@gmail.com>
2022-12-13 23:38:39 +0100

    New

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
-## A simple interactive quiz with python
+## A simple interactive Github and GIT quiz with python
 
-print("Welcome to the Python Quiz!")
+print("Welcome GitHub Quiz!")
 
 score = 0
 
-question_1 = input("What is the capital of India?\nA. Mumbai\nB. Delhi\nC. Bangalore\n")
+question_1 = input("What is another name for Repository in GitHub?\nA. Mainfile\nB. File\nC. Folder\n")
 
-if question_1 == "B":
-    score += 1
+if question_1 == "C" or "b":
+    score += 10
     print("Correct!")
 else:
-    print("Incorrect. The correct answer is Delhi.")
+    print("Incorrect. The correct answer is Folder.")
 
-question_2 = input("What is the currency of India?\nA. Rupee\nB. Dollar\nC. Euro\n")
+question_2 = input("What is GIT ?\nA. It is a CMD app\nB. version control system\nC. A coding storage\n")
 
 if question_2 == "A":
-    score += 1
+    score += 10
     print("Correct!")
 else:
-    print("Incorrect. The correct answer is Rupee.")
+    print("Incorrect. The correct answer is version control system.")
 
 question_3 = input("In what year did India gain independence?\nA. 1947\nB. 1950\nC. 1952\n")
 
 if question_3 == "A":
-    score += 1
+    score += 10
     print("Correct!")
 else:
     print("Incorrect. The correct answer is 1947.")
 
 print("Your final score is:", score)
+
+input = input("Press the Enter key to exit")
