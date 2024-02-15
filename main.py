16bf3c59613319472c389de08a44b1f8c54cdb30
Fishon Amos <43862685+fishonamos@users.noreply.github.com>
2024-02-15 16:52:50 +0100

    Update main.py
    added function to automatically capitalise all inputs.

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ print("Welcome GitHub Quiz!")
 
 score = 0
 
-question_1 = input("What is another name for Repository in GitHub?\nA. Mainfile\nB. File\nC. Folder\n")
+question_1 = input("What is another name for Repository in GitHub?\nA. Mainfile\nB. File\nC. Folder\n").upper()
 
-if question_1 == "C" or "b":
+if question_1 == "C":
     score += 10
     print("Correct!")
 else:
     print("Incorrect. The correct answer is Folder.")
 
-question_2 = input("What is GIT ?\nA. It is a CMD app\nB. version control system\nC. A coding storage\n")
+question_2 = input("What is GIT ?\nA. It is a CMD app\nB. version control system\nC. A coding storage\n").upper()
 
-if question_2 == "B" or 'b':
+if question_2 == "B":
     score += 10
     print("Correct!")
 else:
     print("Incorrect. The correct answer is version control system.")
 
-question_3 = input("How can you stage a new file on GIT?\nA. git add [filename]\nB. git commmit -m 'message'\nC. git shiftx\n")
+question_3 = input("How can you stage a new file on GIT?\nA. git add [filename]\nB. git commmit -m 'message'\nC. git shiftx\n").upper()
 
-if question_3 == "A" or 'a':
+if question_3 == "A":
     score += 10
     print("Correct!")
 else:
